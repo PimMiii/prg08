01187137c7f82f9f1175deeaf5f3c00df9d54de2
PimMiii <pim.milt@gmail.com>
2023-03-28 15:41:11 +0200

    start week7

---
+++ week7/Weekopdracht/train.js
@@ -0,0 +1,22 @@
+import { createChart, updateChart } from "./scatterplot.js"
+
+//
+// demo data
+//
+const data = [
+        { horsepower: 130, mpg: 18 },
+        { horsepower: 165, mpg: 15 },
+        { horsepower: 225, mpg: 14 },
+        { horsepower: 97, mpg: 18 },
+        { horsepower: 88, mpg: 27 },
+        { horsepower: 193, mpg: 9 },
+        { horsepower: 80, mpg: 25 },
+]
+
+const chartdata = data.map(car => ({
+        x: car.horsepower,
+        y: car.mpg,
+       }))
+
+createChart(chartdata)
+
