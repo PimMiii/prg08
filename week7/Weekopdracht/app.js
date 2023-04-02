1e843e8d846e004069449646497c5d3463bc55f2
Pim <30662877+PimMiii@users.noreply.github.com>
2023-04-02 21:50:05 +0200

    weekopdracht done-ish

--- week7/Weekopdracht/app.js
+++ week7/Weekopdracht/app.js
@@ -2,15 +2,21 @@
 // DOM
 //
 const phoneDisplay = document.getElementById("phone");
-const phoneForm = document.getElementById("phoneForm")
-const predictionDisplay = document.getElementById("prediction")
+const phoneForm = document.getElementById("phoneForm");
+const predictionDisplay = document.getElementById("prediction");
+const loadingDisplay = document.getElementById("loading");
 
 const nn = ml5.neuralNetwork({ task: 'regression' })
 
 
 function loadModel() {
     nn.load('./model/model.json', modelLoaded)
+}
 
+function modelLoaded() {
+
+    loadingDisplay.hidden = true;
+    phoneForm.hidden = false;
     phoneForm.addEventListener("submit", (e) => {
         e.preventDefault();
         getPhone(e.target)
@@ -21,16 +27,34 @@ function loadModel() {
 
 function getPhone(form) {
     let phone = {};
-    phone["battery"] = form["battery"].value;
-    phone["cores"] = form["cores"].value;
-    phone["cpu"] = form["cpu"].value;
-    phone["memory"] = form["memory"].value;
-    phone["ppi"] = form["ppi"].value;
-    phone["rearcam"] = form["rearcam"].value;
-    phone["storage"] = form["storage"].value;
-    phone["thickness"] = form["thickness"].value;
+    phone["battery"] = parseInt(form["battery"].value);
+    phone["cores"] = parseInt(form["cores"].value);
+    phone["cpu"] = parseInt(form["cpu"].value);
+    phone["memory"] = parseInt(form["memory"].value);
+    phone["ppi"] = parseInt(form["ppi"].value);
+    phone["rearcam"] = parseInt(form["rearcam"].value);
+    phone["storage"] = parseInt(form["storage"].value);
+    phone["thickness"] = parseInt(form["thickness"].value);
+    phoneDisplay.innerHTML = JSON.stringify(phone);
     console.log(phone)
     makePrediction(phone)
+}
+
+async function makePrediction(phone){
+    predictionDisplay.innerText = "Price: Predicting ...."
+    let prediction = await nn.predict({
+                        ppi: phone.ppi,
+                        battery: phone.battery,
+                        cores: phone.cores,
+                        cpu: phone.cpu,
+                        memory: phone.memory,
+                        storage: phone.storage,
+                        rearcam: phone.rearcam,
+                        thickness: phone.thickness
+    })
+    console.log(prediction)
+    predictionDisplay.innerHTML = `Price: ${Math.floor(prediction[0].price)}`
+    
 }
 
 
